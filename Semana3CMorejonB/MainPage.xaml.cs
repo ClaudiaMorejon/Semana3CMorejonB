772d4a4a1c0e621c8828b0487e7f33291ce17b9d
ClaudiaMorejon <claudia.github@gmail.com>
2020-10-23 21:13:21 -0500

    Agregar archivos de proyecto.

---
+++ Semana3CMorejonB/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Semana3CMorejonB
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnAbrir_ventana_dos_Clicked(object sender, EventArgs e)
+        {
+            string user = txtUser.Text;
+            string password = txtPassword.Text;
+
+            await Navigation.PushAsync(new Ventana2(user,password));
+        }
+    }
+}
---
+++ Semana3CMorejonB/Ventana2.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Semana3CMorejonB
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Ventana2 : ContentPage
+    {
+        public Ventana2(string user, string password)
+        {
+            InitializeComponent();
+            lblUser.Text = user;
+            txtPassword.Text = password;
+        }
+    }
+}
